e721428970afe95d6d79070932ccb0754b7052ee
Jose Moreno <morenojr90@yahoo.com>
2018-06-19 15:24:02 -0700

    finish saving

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ console.log(message);
 function getProfile(username) {
   try {
   // Connect to the API URL (https://teamtreehouse.com/username.json)
-  const request = https.get (`teamtreehouse.com/${username}.json`, response => {
+  const request = https.get (`https://teamtreehouse.com/${username}.json`, response => {
       let body = "";
       //Read the data
       response.on('data', data => {
