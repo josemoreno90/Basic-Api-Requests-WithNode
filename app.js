634a7b27a51a3449054e5e496b7cf47648b5c8cd
Jose Moreno <morenojr90@yahoo.com>
2018-06-20 08:29:47 -0700

    End of parsing Errors2

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 //Solution: Use Node.js to connect to Treehouse's API to get info
 const https = require('https');
 
+//Print Error message
+function printError(error) {
+  console.error(error.message);
+}
+
 //function to print message to console
 function printMessage(username, badgeCount, points) {
 const message =  `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
@@ -12,7 +17,7 @@ console.log(message);
 function getProfile(username) {
   try {
   // Connect to the API URL (https://teamtreehouse.com/username.json)
-  const request = https.get (`teamtreehouse.com/${username}.json`, response => {
+  const request = https.get (`https://teamtreehouse.com/${username}.json`, response => {
       let body = "";
       //Read the data
       response.on('data', data => {
@@ -20,17 +25,20 @@ function getProfile(username) {
       })
 
       response.on('end', () => {
-        //Parse the data
-        const profile = JSON.parse(body);
-        //Print the data
-        printMessage(username, profile.badges.length, profile.points.JavaScript);
-
+        try{
+          //Parse the data
+          const profile = JSON.parse(body);
+          //Print the data
+          printMessage(username, profile.badges.length, profile.points.JavaScript);
+        } catch (error) {
+          printError(error);
+        }
       })
 
     })
-    request.on('error', error => console.error(`Problem with request: ${error.message}`));
+    request.on('error', printError);
   } catch (error) {
-    console.error(error.message + "");
+    printError(error);
   }
 }
 
